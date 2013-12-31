4127b41ce13627157bfa5dab63ef16fad7959d6f
Norman Saez <nfsaez@uc.cl>
2013-12-31 11:26:38 -0300

    improve code standalone

--- test/PY/standalonetest.py
+++ test/PY/standalonetest.py
@@ -3,594 +3,130 @@
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
 from time import sleep
-
-def turn_off():
-#    ####################################
-#    GPIO.setup("P8_1", GPIO.OUT)
-#    GPIO.output("P8_1",GPIO.LOW)
-#
-#    ####################################
-#    GPIO.setup("P8_2", GPIO.OUT)
-#    GPIO.output("P8_2",GPIO.LOW)
-#
-#    ####################################
-    GPIO.setup("P8_3", GPIO.OUT)
-    GPIO.output("P8_3",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_4", GPIO.OUT)
-    GPIO.output("P8_4",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_5", GPIO.OUT)
-    GPIO.output("P8_5",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_6", GPIO.OUT)
-    GPIO.output("P8_6",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_7", GPIO.OUT)
-    GPIO.output("P8_7",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_8", GPIO.OUT)
-    GPIO.output("P8_8",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_9", GPIO.OUT)
-    GPIO.output("P8_9",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_10", GPIO.OUT)
-    GPIO.output("P8_10",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_11", GPIO.OUT)
-    GPIO.output("P8_11",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_12", GPIO.OUT)
-    GPIO.output("P8_12",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_13", GPIO.OUT)
-    GPIO.output("P8_13",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_14", GPIO.OUT)
-    GPIO.output("P8_14",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_15", GPIO.OUT)
-    GPIO.output("P8_15",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_16", GPIO.OUT)
-    GPIO.output("P8_16",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_17", GPIO.OUT)
-    GPIO.output("P8_17",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_18", GPIO.OUT)
-    GPIO.output("P8_18",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_18", GPIO.OUT)
-    GPIO.output("P8_18",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_19", GPIO.OUT)
-    GPIO.output("P8_19",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_20", GPIO.OUT)
-    GPIO.output("P8_20",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_20", GPIO.OUT)
-    GPIO.output("P8_20",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_21", GPIO.OUT)
-    GPIO.output("P8_21",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_22", GPIO.OUT)
-    GPIO.output("P8_22",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_23", GPIO.OUT)
-    GPIO.output("P8_23",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_24", GPIO.OUT)
-    GPIO.output("P8_24",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_25", GPIO.OUT)
-    GPIO.output("P8_25",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_26", GPIO.OUT)
-    GPIO.output("P8_26",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_27", GPIO.OUT)
-    GPIO.output("P8_27",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_28", GPIO.OUT)
-    GPIO.output("P8_28",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_29", GPIO.OUT)
-    GPIO.output("P8_29",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_30", GPIO.OUT)
-    GPIO.output("P8_30",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_31", GPIO.OUT)
-    GPIO.output("P8_31",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_32", GPIO.OUT)
-    GPIO.output("P8_32",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_33", GPIO.OUT)
-    GPIO.output("P8_33",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_34", GPIO.OUT)
-    GPIO.output("P8_34",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_35", GPIO.OUT)
-    GPIO.output("P8_35",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_36", GPIO.OUT)
-    GPIO.output("P8_36",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_37", GPIO.OUT)
-    GPIO.output("P8_37",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_38", GPIO.OUT)
-    GPIO.output("P8_38",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P8_39", GPIO.OUT)
-    GPIO.output("P8_39",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_40", GPIO.OUT)
-    GPIO.output("P8_40",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_41", GPIO.OUT)
-    GPIO.output("P8_41",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_42", GPIO.OUT)
-    GPIO.output("P8_42",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_43", GPIO.OUT)
-    GPIO.output("P8_43",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_44", GPIO.OUT)
-    GPIO.output("P8_44",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_45", GPIO.OUT)
-    GPIO.output("P8_45",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P8_46", GPIO.OUT)
-    GPIO.output("P8_46",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_1", GPIO.OUT)
-    GPIO.output("P9_1",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_2", GPIO.OUT)
-    GPIO.output("P9_2",GPIO.LOW)
-
-#    ####################################
-#    GPIO.setup("P9_3", GPIO.OUT)
-#    GPIO.output("P9_3",GPIO.LOW)
-#
-#    ####################################
-#    GPIO.setup("P9_4", GPIO.OUT)
-#    GPIO.output("P9_4",GPIO.LOW)
-#
-#    ####################################
-#    GPIO.setup("P9_5", GPIO.OUT)
-#    GPIO.output("P9_5",GPIO.LOW)
-#
-#    ####################################
-#    GPIO.setup("P9_6", GPIO.OUT)
-#    GPIO.output("P9_6",GPIO.LOW)
-#
-#    ####################################
-#    GPIO.setup("P9_7", GPIO.OUT)
-#    GPIO.output("P9_7",GPIO.LOW)
-#
-#    ####################################
-#    GPIO.setup("P9_8", GPIO.OUT)
-#    GPIO.output("P9_8",GPIO.LOW)
-#
-#    ####################################
-#    GPIO.setup("P9_9", GPIO.OUT)
-#    GPIO.output("P9_9",GPIO.LOW)
-#
-#    ####################################
-#    GPIO.setup("P9_10", GPIO.OUT)
-#    GPIO.output("P9_10",GPIO.LOW)
-#
-#    ####################################
-    GPIO.setup("P9_11", GPIO.OUT)
-    GPIO.output("P9_11",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_12", GPIO.OUT)
-    GPIO.output("P9_12",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_13", GPIO.OUT)
-    GPIO.output("P9_13",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_14", GPIO.OUT)
-    GPIO.output("P9_14",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_15", GPIO.OUT)
-    GPIO.output("P9_15",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_16", GPIO.OUT)
-    GPIO.output("P9_16",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_17", GPIO.OUT)
-    GPIO.output("P9_17",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_18", GPIO.OUT)
-    GPIO.output("P9_18",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_18", GPIO.OUT)
-    GPIO.output("P9_18",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_19", GPIO.OUT)
-    GPIO.output("P9_19",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_20", GPIO.OUT)
-    GPIO.output("P9_20",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_20", GPIO.OUT)
-    GPIO.output("P9_20",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_21", GPIO.OUT)
-    GPIO.output("P9_21",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_22", GPIO.OUT)
-    GPIO.output("P9_22",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_23", GPIO.OUT)
-    GPIO.output("P9_23",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_24", GPIO.OUT)
-    GPIO.output("P9_24",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_25", GPIO.OUT)
-    GPIO.output("P9_25",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_26", GPIO.OUT)
-    GPIO.output("P9_26",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_27", GPIO.OUT)
-    GPIO.output("P9_27",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_28", GPIO.OUT)
-    GPIO.output("P9_28",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_29", GPIO.OUT)
-    GPIO.output("P9_29",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_30", GPIO.OUT)
-    GPIO.output("P9_30",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_31", GPIO.OUT)
-    GPIO.output("P9_31",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_32", GPIO.OUT)
-    GPIO.output("P9_32",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_33", GPIO.OUT)
-    GPIO.output("P9_33",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_34", GPIO.OUT)
-    GPIO.output("P9_34",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_35", GPIO.OUT)
-    GPIO.output("P9_35",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_36", GPIO.OUT)
-    GPIO.output("P9_36",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_37", GPIO.OUT)
-    GPIO.output("P9_37",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_38", GPIO.OUT)
-    GPIO.output("P9_38",GPIO.LOW)
-
-    ####################################
-    GPIO.setup("P9_39", GPIO.OUT)
-    GPIO.output("P9_39",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_40", GPIO.OUT)
-    GPIO.output("P9_40",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_41", GPIO.OUT)
-    GPIO.output("P9_41",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_42", GPIO.OUT)
-    GPIO.output("P9_42",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_43", GPIO.OUT)
-    GPIO.output("P9_43",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_44", GPIO.OUT)
-    GPIO.output("P9_44",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_45", GPIO.OUT)
-    GPIO.output("P9_45",GPIO.LOW)
-
-
-    ####################################
-    GPIO.setup("P9_46", GPIO.OUT)
-    GPIO.output("P9_46",GPIO.LOW)
+import sys
+
+def turn_on_gpio(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin,GPIO.HIGH)
+    print sys._getframe().f_code.co_name,
+    print(' '+pin)
+    raw_input('press a key to continue')
+
+def turn_off_gpio(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin,GPIO.LOW)
+    print sys._getframe().f_code.co_name,
+    print(' '+pin)
+    raw_input('press a key to continue')
+
+def turn_on_pwm(pin):
+    PWM.start(pin, 50)
+    PWM.set_duty_cycle(pin, 25.5)
+    PWM.set_frequency(pin, 10)
+    print sys._getframe().f_code.co_name,
+    print(' '+pin)
+    raw_input('press a key to continue')
+
+def turn_off_pwm(pin):
+    PWM.stop(pin)
+    PWM.cleanup()
+    print sys._getframe().f_code.co_name,
+    print(' '+pin)
+    raw_input('press a key to continue')
+
+if __name__ == '__main__':
+    turn_off_gpio("P8_3")
+    turn_off_gpio("P8_4")
+    turn_off_gpio("P8_5")
+    turn_off_gpio("P8_6")
+    turn_off_gpio("P8_7")
+    turn_off_gpio("P8_8")
+    turn_off_gpio("P8_9")
+    turn_off_gpio("P8_10")
+    turn_off_gpio("P8_11")
+    turn_off_gpio("P8_12")
+    turn_off_gpio("P8_13")
+    turn_off_gpio("P8_14")
+    turn_off_gpio("P8_15")
+    turn_off_gpio("P8_16")
+    turn_off_gpio("P8_17")
+    turn_off_gpio("P8_18")
+    turn_off_gpio("P8_18")
+    turn_off_gpio("P8_19")
+    turn_off_gpio("P8_20")
+    turn_off_gpio("P8_20")
+    turn_off_gpio("P8_21")
+    turn_off_gpio("P8_22")
+    turn_off_gpio("P8_23")
+    turn_off_gpio("P8_24")
+    turn_off_gpio("P8_25")
+    turn_off_gpio("P8_26")
+    turn_off_gpio("P8_27")
+    turn_off_gpio("P8_28")
+    turn_off_gpio("P8_29")
+    turn_off_gpio("P8_30")
+    turn_off_gpio("P8_31")
+    turn_off_gpio("P8_32")
+    turn_off_gpio("P8_33")
+    turn_off_gpio("P8_34")
+    turn_off_gpio("P8_35")
+    turn_off_gpio("P8_36")
+    turn_off_gpio("P8_37")
+    turn_off_gpio("P8_38")
+    turn_off_gpio("P8_39")
+    turn_off_gpio("P8_40")
+    turn_off_gpio("P8_41")
+    turn_off_gpio("P8_42")
+    turn_off_gpio("P8_43")
+    turn_off_gpio("P8_44")
+    turn_off_gpio("P8_45")
+    turn_off_gpio("P8_46")
+    turn_off_gpio("P9_1")
+    turn_off_gpio("P9_2")
+#    turn_off_gpio("P9_3")
+#    turn_off_gpio("P9_4")
+#    turn_off_gpio("P9_5")
+#    turn_off_gpio("P9_6")
+#    turn_off_gpio("P9_7")
+#    turn_off_gpio("P9_8")
+#    turn_off_gpio("P9_9")
+#    turn_off_gpio("P9_10")
+    turn_off_gpio("P9_11")
+    turn_off_gpio("P9_12")
+    turn_off_gpio("P9_13")
+    turn_off_gpio("P9_14")
+    turn_off_gpio("P9_15")
+    turn_off_gpio("P9_16")
+    turn_off_gpio("P9_17")
+    turn_off_gpio("P9_18")
+    turn_off_gpio("P9_18")
+    turn_off_gpio("P9_19")
+    turn_off_gpio("P9_20")
+    turn_off_gpio("P9_20")
+    turn_off_gpio("P9_21")
+    turn_off_gpio("P9_22")
+    turn_off_gpio("P9_23")
+    turn_off_gpio("P9_24")
+    turn_off_gpio("P9_25")
+    turn_off_gpio("P9_26")
+    turn_off_gpio("P9_27")
+    turn_off_gpio("P9_28")
+    turn_off_gpio("P9_29")
+    turn_off_gpio("P9_30")
+    turn_off_gpio("P9_31")
+    turn_off_gpio("P9_32")
+    turn_off_gpio("P9_33")
+    turn_off_gpio("P9_34")
+    turn_off_gpio("P9_35")
+    turn_off_gpio("P9_36")
+    turn_off_gpio("P9_37")
+    turn_off_gpio("P9_38")
+    turn_off_gpio("P9_39")
+    turn_off_gpio("P9_40")
+    turn_off_gpio("P9_41")
+    turn_off_gpio("P9_42")
+    turn_off_gpio("P9_43")
+    turn_off_gpio("P9_44")
+    turn_off_gpio("P9_45")
+    turn_off_gpio("P9_46")
     print "finish to turn off"
-
-timeout = 3 #secs
-
-print "NGS"
-print "Connector A"
-
-turn_off()
-####################################
-print "1"
-GPIO.setup("P8_20", GPIO.OUT)
-GPIO.setup("P9_42", GPIO.OUT)
-GPIO.output("P8_20",GPIO.HIGH)
-GPIO.output("P9_42",GPIO.HIGH)
-PWM.start("P9_16", 50)
-PWM.set_duty_cycle("P9_16", 25.5)
-PWM.set_frequency("P9_16", 10)
-sleep(timeout)
-PWM.stop("P9_16")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "2"
-GPIO.setup("P8_22", GPIO.OUT)
-GPIO.setup("P9_42", GPIO.OUT)
-GPIO.output("P8_22",GPIO.HIGH)
-GPIO.output("P9_42",GPIO.HIGH)
-PWM.start("P9_16", 50)
-PWM.set_duty_cycle("P9_16", 25.5)
-PWM.set_frequency("P9_16", 10)
-sleep(timeout)
-PWM.stop("P9_16")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "3"
-GPIO.setup("P8_24", GPIO.OUT)
-GPIO.setup("P9_42", GPIO.OUT)
-GPIO.output("P8_24",GPIO.HIGH)
-GPIO.output("P9_42",GPIO.HIGH)
-PWM.start("P9_16", 50)
-PWM.set_duty_cycle("P9_16", 25.5)
-PWM.set_frequency("P9_16", 10)
-sleep(timeout)
-PWM.stop("P9_16")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "4"
-GPIO.setup("P8_6", GPIO.OUT)
-GPIO.setup("P8_11", GPIO.OUT)
-GPIO.output("P8_6",GPIO.HIGH)
-GPIO.output("P8_11",GPIO.HIGH)
-PWM.start("P8_19", 50)
-PWM.set_duty_cycle("P8_19", 25.5)
-PWM.set_frequency("P8_19", 10)
-sleep(timeout)
-PWM.stop("P8_19")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "5"
-GPIO.setup("P8_12", GPIO.OUT)
-GPIO.setup("P8_11", GPIO.OUT)
-GPIO.output("P8_12",GPIO.HIGH)
-GPIO.output("P8_11",GPIO.HIGH)
-PWM.start("P8_19", 50)
-PWM.set_duty_cycle("P8_19", 25.5)
-PWM.set_frequency("P8_19", 10)
-sleep(timeout)
-PWM.stop("P8_19")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "6"
-GPIO.setup("P8_14", GPIO.OUT)
-GPIO.setup("P8_11", GPIO.OUT)
-GPIO.output("P8_14",GPIO.HIGH)
-GPIO.output("P8_11",GPIO.HIGH)
-PWM.start("P8_19", 50)
-PWM.set_duty_cycle("P8_19", 25.5)
-PWM.set_frequency("P8_19", 10)
-sleep(timeout)
-PWM.stop("P8_19")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "7"
-GPIO.setup("P8_16", GPIO.OUT)
-GPIO.setup("P8_11", GPIO.OUT)
-GPIO.output("P8_16",GPIO.HIGH)
-GPIO.output("P8_11",GPIO.HIGH)
-PWM.start("P8_19", 50)
-PWM.set_duty_cycle("P8_19", 25.5)
-PWM.set_frequency("P8_19", 10)
-sleep(timeout)
-PWM.stop("P8_19")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "8"
-GPIO.setup("P8_18", GPIO.OUT)
-GPIO.setup("P8_11", GPIO.OUT)
-GPIO.output("P8_18",GPIO.HIGH)
-GPIO.output("P8_11",GPIO.HIGH)
-PWM.start("P8_19", 50)
-PWM.set_duty_cycle("P8_19", 25.5)
-PWM.set_frequency("P8_19", 10)
-sleep(timeout)
-PWM.stop("P8_19")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "9"
-GPIO.setup("P8_20", GPIO.OUT)
-GPIO.setup("P8_11", GPIO.OUT)
-GPIO.output("P8_20",GPIO.HIGH)
-GPIO.output("P8_11",GPIO.HIGH)
-PWM.start("P8_19", 50)
-PWM.set_duty_cycle("P8_19", 25.5)
-PWM.set_frequency("P8_19", 10)
-sleep(timeout)
-PWM.stop("P8_19")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "10"
-GPIO.setup("P8_22", GPIO.OUT)
-GPIO.setup("P8_11", GPIO.OUT)
-GPIO.output("P8_22",GPIO.HIGH)
-GPIO.output("P8_11",GPIO.HIGH)
-PWM.start("P8_19", 50)
-PWM.set_duty_cycle("P8_19", 25.5)
-PWM.set_frequency("P8_19", 10)
-sleep(timeout)
-PWM.stop("P8_19")
-PWM.cleanup()
-GPIO.cleanup()
-
-turn_off()
-####################################
-print "11"
-GPIO.setup("P8_24", GPIO.OUT)
-GPIO.setup("P8_11", GPIO.OUT)
-GPIO.output("P8_24",GPIO.HIGH)
-GPIO.output("P8_11",GPIO.HIGH)
-PWM.start("P8_19", 50)
-PWM.set_duty_cycle("P8_19", 25.5)
-PWM.set_frequency("P8_19", 10)
-sleep(timeout)
-PWM.stop("P8_19")
-PWM.cleanup()
-GPIO.cleanup()
-
