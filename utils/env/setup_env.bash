4d8b7fe52cd4d1f714324788dff242ff3b745cba
Norman Saez <nfsaez@uc.cl>
2014-01-03 01:29:59 +0000

    adding sudoers and bashrc avoid insert unnecesary setup

--- utils/env/setup_env.bash
+++ utils/env/setup_env.bash
@@ -1,4 +1,7 @@
-cat bashrc >>  ~/.bashrc
-cp gitconfig ~/.gitconfig
-cp pythonrc ~/.pythonrc
-cp vimrc ~/.vimrc
+cp -v bashrc  ~/.bashrc
+cp -v gitconfig ~/.gitconfig
+cp -v pythonrc ~/.pythonrc
+cp -v vimrc ~/.vimrc
+if [ "$USER" = "root" ]; then
+    cp -v sudoers /etc/sudoers
+fi
