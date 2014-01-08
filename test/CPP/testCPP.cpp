db994c0f894f47493387177a9487ddca9ac84ca4
Norman Saez <nfsaez@uc.cl>
2014-01-08 16:15:41 -0300

    Update to move a motor in C++

--- test/CPP/testCPP.cpp
+++ test/CPP/testCPP.cpp
@@ -9,93 +9,78 @@ int  main (void)
     //define file handles
     FILE *ofp_export, *ofp_P8_13_value, *ofp_P8_13_direction;
     //define pin variables
-    int pin_dir =117, logic_status = 1;
+    int logic_status = 0;
     int stop = 0;
     char* pin_direction = output;
-    //establish a direction and value file within export for P8_13
+
+    //EXPORTs
     ofp_export = fopen("/sys/class/gpio/export", "w");
-    if(ofp_export == NULL) {printf("Unable to open export.\n");}
+    if(ofp_export == NULL) {printf("Unable to open export. 88\n");}
     fseek(ofp_export, 0, SEEK_SET);
     fprintf(ofp_export, "%d", 88);
     fflush(ofp_export);
-        fclose(ofp_export);
+    fclose(ofp_export);
 
 
-    //establish a direction and value file within export for P8_13
     ofp_export = fopen("/sys/class/gpio/export", "w");
-    if(ofp_export == NULL) {printf("Unable to open export.\n");}
+    if(ofp_export == NULL) {printf("Unable to open export. 86\n");}
     fseek(ofp_export, 0, SEEK_SET);
     fprintf(ofp_export, "%d",86);
     fflush(ofp_export);
-        fclose(ofp_export);
+    fclose(ofp_export);
 
 
-    //establish a direction and value file within export for P8_13
     ofp_export = fopen("/sys/class/gpio/export", "w");
-    if(ofp_export == NULL) {printf("Unable to open export.\n");}
+    if(ofp_export == NULL) {printf("Unable to open export. 32\n");}
     fseek(ofp_export, 0, SEEK_SET);
     fprintf(ofp_export, "%d",32);
     fflush(ofp_export);
-        fclose(ofp_export);
+    fclose(ofp_export);
 
 
-    //establish a direction and value file within export for P8_13
-    ofp_export = fopen("/sys/class/gpio/export", "w");
-    if(ofp_export == NULL) {printf("Unable to open export.\n");}
-    fseek(ofp_export, 0, SEEK_SET);
-    fprintf(ofp_export, "%d",74);
-    fflush(ofp_export);
-        fclose(ofp_export);
 
-
-    //establish a direction and value file within export for P8_13
-    ofp_export = fopen("/sys/class/gpio/export", "w");
-    if(ofp_export == NULL) {printf("Unable to open export.\n");}
-    fseek(ofp_export, 0, SEEK_SET);
-    fprintf(ofp_export, "%d",72);
-    fflush(ofp_export);
-        fclose(ofp_export);
-
-    //configure P8_13 for writing
+    //PUT AS OUTPUT
     ofp_P8_13_direction = fopen("/sys/class/gpio/gpio88/direction", "w");
-    if(ofp_P8_13_direction==NULL){printf("Unable to open P8_13_direction.\n");}
+    if(ofp_P8_13_direction==NULL){printf("Unable to open. 88\n");}
     fseek(ofp_P8_13_direction, 0, SEEK_SET);
     fprintf(ofp_P8_13_direction, "%s",  pin_direction);
     fflush(ofp_P8_13_direction);
-        fclose(ofp_P8_13_direction);
-    //configure P8_13 for writing
+    fclose(ofp_P8_13_direction);
+    //
     ofp_P8_13_direction = fopen("/sys/class/gpio/gpio86/direction", "w");
-    if(ofp_P8_13_direction==NULL){printf("Unable to open P8_13_direction.\n");}
+    if(ofp_P8_13_direction==NULL){printf("Unable to open. 86\n");}
     fseek(ofp_P8_13_direction, 0, SEEK_SET);
     fprintf(ofp_P8_13_direction, "%s",  pin_direction);
     fflush(ofp_P8_13_direction);
-        fclose(ofp_P8_13_direction);
-    //configure P8_13 for writing
+    fclose(ofp_P8_13_direction);
+    //
     ofp_P8_13_direction = fopen("/sys/class/gpio/gpio32/direction", "w");
-    if(ofp_P8_13_direction==NULL){printf("Unable to open P8_13_direction.\n");}
+    if(ofp_P8_13_direction==NULL){printf("Unable to open. 32\n");}
     fseek(ofp_P8_13_direction, 0, SEEK_SET);
     fprintf(ofp_P8_13_direction, "%s",  pin_direction);
     fflush(ofp_P8_13_direction);
-        fclose(ofp_P8_13_direction);
-    //configure P8_13 for writing
-    ofp_P8_13_direction = fopen("/sys/class/gpio/gpio74/direction", "w");
-    if(ofp_P8_13_direction==NULL){printf("Unable to open P8_13_direction.\n");}
-    fseek(ofp_P8_13_direction, 0, SEEK_SET);
-    fprintf(ofp_P8_13_direction, "%s",  pin_direction);
-    fflush(ofp_P8_13_direction);
-        fclose(ofp_P8_13_direction);
-    //configure P8_13 for writing
-    ofp_P8_13_direction = fopen("/sys/class/gpio/gpio72/direction", "w");
-    if(ofp_P8_13_direction==NULL){printf("Unable to open P8_13_direction.\n");}
-    fseek(ofp_P8_13_direction, 0, SEEK_SET);
-    fprintf(ofp_P8_13_direction, "%s",  pin_direction);
-    fflush(ofp_P8_13_direction);
-        fclose(ofp_P8_13_direction);
+    fclose(ofp_P8_13_direction);
+
+    //PUT SLEEP IN HIGH
+    ofp_P8_13_value = fopen("/sys/class/gpio/gpio32/value", "w");
+    if(ofp_P8_13_value == NULL) {printf("Unable to open. 32\n");}
+    fseek(ofp_P8_13_value, 0, SEEK_SET);
+    fprintf(ofp_P8_13_value, "%d", logic_status);
+    fflush(ofp_P8_13_value);
+    fclose(ofp_P8_13_value);
+    
+    //CHANGE DIRECTION T0 0
+    ofp_P8_13_value = fopen("/sys/class/gpio/gpio88/value", "w");
+    if(ofp_P8_13_value == NULL) {printf("Unable to open.\n");}
+    fseek(ofp_P8_13_value, 0, SEEK_SET);
+    fprintf(ofp_P8_13_value, "%d",0);
+    fflush(ofp_P8_13_value);
+    fclose(ofp_P8_13_value);
 
     while(stop == 5000){
-        //write a logic 1 to P8_13 to illuminate the LED
+        //PUT STEP in HIGH and LOW 
         ofp_P8_13_value = fopen("/sys/class/gpio/gpio86/value", "w");
-        if(ofp_P8_13_value == NULL) {printf("Unable to open gpio117_value.\n");}
+        if(ofp_P8_13_value == NULL) {printf("Unable to open. 86\n");}
         fseek(ofp_P8_13_value, 0, SEEK_SET);
         fprintf(ofp_P8_13_value, "%d", logic_status);
         fflush(ofp_P8_13_value);
@@ -106,18 +91,18 @@ int  main (void)
         stop += 1;
     }
 
-    //configure P8_13 for writing
-    ofp_P8_13_direction = fopen("/sys/class/gpio/gpio88/direction", "w");
-    if(ofp_P8_13_direction==NULL){printf("Unable to open P8_13_direction.\n");}
-    fseek(ofp_P8_13_direction, 0, SEEK_SET);
-    fprintf(ofp_P8_13_direction, "%s",);
-    fflush(ofp_P8_13_direction);
-        fclose(ofp_P8_13_direction);
+    //CHANGE DIRECTION TO OPOSIT DIRECTION
+    ofp_P8_13_value = fopen("/sys/class/gpio/gpio88/value", "w");
+    if(ofp_P8_13_value == NULL) {printf("Unable to open.\n");}
+    fseek(ofp_P8_13_value, 0, SEEK_SET);
+    fprintf(ofp_P8_13_value, "%d",1);
+    fflush(ofp_P8_13_value);
+    fclose(ofp_P8_13_value);
     stop = 0;
     while(stop == 5000){
-        //write a logic 1 to P8_13 to illuminate the LED
+        //PUT STEP in HIGH and LOW 
         ofp_P8_13_value = fopen("/sys/class/gpio/gpio86/value", "w");
-        if(ofp_P8_13_value == NULL) {printf("Unable to open gpio117_value.\n");}
+        if(ofp_P8_13_value == NULL) {printf("Unable to open. 86\n");}
         fseek(ofp_P8_13_value, 0, SEEK_SET);
         fprintf(ofp_P8_13_value, "%d", logic_status);
         fflush(ofp_P8_13_value);
@@ -127,7 +112,6 @@ int  main (void)
         sleep(5);
         stop += 1;
     }
-
 
     return 1;
 }
