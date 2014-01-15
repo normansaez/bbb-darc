f3b0c3b2fe7fbb7664accb7849cdcc64abfe252e
Nicolas Dubost <nsdubost@uc.cl>
2014-01-15 17:14:19 -0300

    SetuseBrightest.py done. Need testing

--- calibrations/SetuseBrightest.py
+++ calibrations/SetuseBrightest.py
@@ -2,12 +2,33 @@
 import FITS
 import darc
 import numpy
+import pylab
 
 c=darc.Control("ShackHartmann")
 #cent = c.Get("refCentroids")
 # setear centroides de referencia on-axis c.Set("refCentroids",None)
-niter = 500
-cent = c.GetStreamBlock(cameraName+'rtcPxlBuf',niter)#niter frames - as a list
+niter = 200
+nsubaps = 500                                               # number of subaps
+nBrightest = 100                                            # range of values
+                                                            # to test
+noise = numpy.zeros(nBrightest)
+for i in range(1,nBrightest+1)
+    c.Set('useBrightest',-i)
+    cent = c.GetStreamBlock(cameraName+'rtcCentBuf',niter)   # niter frames - as a list
+    centx2 = numpy.square(cent[:,::2])
+    centy2 = numpy.square(cent[:,1::2])
+    noise[i] = ((centx+centy).sum(0)/float(niter)).sum(0)/float(nsubaps)       # 
+
+c.Set('useBrightest',noise.argmin(0))
+print noise.argmin(0)
+pylab.plot(noise)
+pylab.show()
 
 FITS.Write(cent.astype(numpy.float32),'cent_led_1.fits')
+
+
+
+
+
+
 
