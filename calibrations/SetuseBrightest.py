fa8e40f73b0f232bd7e8d9786fe0fa169b848e64
Nicolas Dubost <nsdubost@uc.cl>
2014-01-24 18:15:41 -0300

    Version up and running

--- calibrations/SetuseBrightest.py
+++ calibrations/SetuseBrightest.py
@@ -6,20 +6,26 @@ import pylab
 
 c=darc.Control("ShackHartmann")
 # funcion de prender led que setee bg, refs, subaps y todo
-# con el shutter correcto. Si todo ya está seteado y la GUI ya prendió el led,
+# con el shutter correcto. Si todo ya esta seteado y la GUI ya prendio el led,
 # entonces no-problemo
 # c.Set("refCentroids",None)
-niter = 200
-nsubaps = 500                                               # number of subaps
+niter = 100
+nsubaps = 416                                               # number of subaps
 nBrightest = 100                                            # range of values
                                                             # to test
 noise = numpy.zeros(nBrightest)
-for i in range(1,nBrightest+1)
+cameraName = 'ShackHartmann'
+frames = numpy.zeros([niter,nsubaps])
+for i in range(1,nBrightest+1):
+    print '\nRecording with useBrightest:%3.0f ' %i
     c.Set('useBrightest',-i)
-    cent = c.GetStreamBlock(cameraName+'rtcCentBuf',niter)   # niter frames - as a list
-    centx2 = numpy.square(cent[:,::2])
-    centy2 = numpy.square(cent[:,1::2])
-    noise[i] = ((centx+centy).sum(0)/float(niter)).sum(0)/float(nsubaps)       # 
+    cent = c.GetStreamBlock(cameraName+'rtcCentBuf',niter)   # niter frames - as a dict
+    cent = cent[cent.keys()[0]]
+    for j in range(0,niter):
+        frames[j,:] = cent[j][0]
+    centx2 = numpy.square(frames[:,::2])
+    centy2 = numpy.square(frames[:,1::2])
+    noise[i] = ((centx2+centy2).sum(0)/float(niter)).sum(0)/float(nsubaps)       #
 
 c.Set('useBrightest',noise.argmin(0))
 print noise.argmin(0)
