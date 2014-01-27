91ff8ff0b461947a3fe5a2c818aea359bd2d34d0
Nicolas Dubost <nsdubost@uc.cl>
2014-01-27 16:11:10 -0300

    First try to get static slope noise

--- calibrations/GetSlopeNoise.py
+++ calibrations/GetSlopeNoise.py
@@ -5,11 +5,24 @@ import numpy
 import pylab
 
 c=darc.Control("ShackHartmann")
-niter = 5000 
-cent = c.GetStreamBlock(cameraName+'rtcCentBuf',niter)   # niter frames - as a list
-FITS.Write(cent.astype(numpy.float32),'Slopes_Noise.fits')
-pylab.plot(cent)
-pylab.show()
+niter = 5000
+nsubaps = 416                                            
+c.Set('useBrightest',-85)
+                                          
+noise = numpy.zeros(nBrightest)
+cameraName = 'ShackHartmann'
+frames = numpy.zeros([niter,nsubaps])
+
+cent = c.GetStreamBlock(cameraName+'rtcCentBuf',niter)   # niter frames - as a dict
+cent = cent[cent.keys()[0]]
+for j in range(0,niter):
+    frames[j,:] = cent[j][0]
+
+#pylab.plot(noise)
+#pylab.show()
+
+FITS.Write(frames.astype(numpy.float32),'static_slope_noise.fits')
+
 
 
 
