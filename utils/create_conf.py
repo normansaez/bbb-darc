b2ec9c63ea7e1d970de3a158edfd1e28319d8f8e
Norman Saez <nfsaez@uc.cl>
2014-03-03 17:12:15 -0300

    Creating configuration files and dicts

--- utils/create_conf.py
+++ utils/create_conf.py
@@ -1,69 +1,83 @@
 import csv
+import os
 
-print "[beagledarc_server]"
-print "host = 192.168.7.2"
-print "user = root"
-print "password = "
-print "port = "
-print "ior = IOR:010000001900000049444c3a4242425365727665722f5365727665723a312e3000000000010000000000000064000000010102000b00000031302e34322e302e3937000052e700000e000000fed0a5cc5200000b99000000000000000200000000000000080000000100000000545441010000001c00000001000000010001000100000001000105090101000100000009010100 "
-print " "
-print "[darc]"
-print "camera = ShackHartmann"
-print "pxlx = 1920"
-print "pxly = 1080"
-print "image_path = /home/dani/nsaez/images/"
-print 
+filename_cfg = "configurations.cfg"
+filename_dic = "server_dic.py"
+
+filehandler_cfg = open(filename_cfg, 'w')
+filehandler_dic = open(filename_dic, 'w')
+
+filehandler_cfg.write("[beagledarc_server]\n")
+filehandler_cfg.write("host = 192.168.7.2\n")
+filehandler_cfg.write("user = root\n")
+filehandler_cfg.write("password = \n")
+filehandler_cfg.write("port = \n")
+filehandler_cfg.write("ior = IOR:010000001900000049444c3a4242425365727665722f5365727665723a312e3000000000010000000000000064000000010102000b00000031302e34322e302e3937000052e700000e000000fed0a5cc5200000b99000000000000000200000000000000080000000100000000545441010000001c00000001000000010001000100000001000105090101000100000009010100 \n")
+filehandler_cfg.write("\n")
+filehandler_cfg.write("[darc]\n")
+filehandler_cfg.write("camera = ShackHartmann\n")
+filehandler_cfg.write("pxlx = 1920\n")
+filehandler_cfg.write("pxly = 1080\n")
+filehandler_cfg.write("image_path = /home/dani/nsaez/images/\n")
+filehandler_cfg.write("\n")
 
 with open('mapping.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         if row[1].__contains__('m') and not row[1].__contains__('name'):
             if row[1] == 'm1':
-                print "[ground_layer]"
-                print "name = ground_layer"
+                filehandler_cfg.write("[ground_layer]\n")
+                filehandler_cfg.write("name = ground_layer\n")
             if row[1] == 'm2':
-                print "[vertical_altitude_layer]"
-                print "name = vertical_altitude_layer"
+                filehandler_cfg.write("[vertical_altitude_layer]\n")
+                filehandler_cfg.write("name = vertical_altitude_layer\n")
             if row[1] == 'm3':
-                print "[horizontal_altitude_layer]"
-                print "name = horizontal_altitude_layer"
-            print "pin_dir = %s" % row[2]
-            print "pin_step = %s" % row[3]
-            print "pin_sleep = %s" % row[4]
-            print "pin_opto1 = %s" % row[5]
-            print "pin_opto2 = %s" % row[6]
-            print "simulated = False"
-            print "direction = INIT_POSITION"
-            print "velocity = 57"
-            print "steps = 50000"
-            print "vr_init = 2"
-            print "vr_end = 3"
-            print "cur_pos = 0"
-            print "image_prefix = %s" % row[1]
-            print
+                filehandler_cfg.write("[horizontal_altitude_layer]\n")
+                filehandler_cfg.write("name = horizontal_altitude_layer\n")
+            filehandler_cfg.write("pin_dir = %s\n" % row[2])
+            filehandler_cfg.write("pin_step = %s\n" % row[3])
+            filehandler_cfg.write("pin_sleep = %s\n" % row[4])
+            filehandler_cfg.write("pin_opto1 = %s\n" % row[5])
+            filehandler_cfg.write("pin_opto2 = %s\n" % row[6])
+            filehandler_cfg.write("simulated = False\n")
+            filehandler_cfg.write("direction = INIT_POSITION\n")
+            filehandler_cfg.write("velocity = 57\n")
+            filehandler_cfg.write("steps = 50000\n")
+            filehandler_cfg.write("vr_init = 0\n")
+            filehandler_cfg.write("vr_end = 21000\n")
+            filehandler_cfg.write("cur_pos = 0\n")
+            filehandler_cfg.write("image_prefix = %s\n" % row[1])
+            filehandler_cfg.write("\n")
         else:
             if row[0] == '100' or row[0] == '0' or row[0] == 'id' or row[0] == "":
                 pass
             else:
-                print "[led_%s]" % row[0]
-                print "pin_led = %s" % row[2]
-                print "pin_pwm = %s" % row[3]
-                print "pin_enable = %s" % row[4]
-                print "name = %s" % row[1]
-                print "simulated = False"
-                print "exp_time = 15000.0"
-                print "brightness = 94"
-                print "image_prefix = %s" % row[0]
-                print
+                filehandler_cfg.write("[led_%s]\n" % row[0])
+                filehandler_cfg.write("pin_led = %s\n" % row[2])
+                filehandler_cfg.write("pin_pwm = %s\n" % row[3])
+                filehandler_cfg.write("pin_enable = %s\n" % row[4])
+                filehandler_cfg.write("name = %s\n" % row[1])
+                filehandler_cfg.write("simulated = False\n")
+                filehandler_cfg.write("exp_time = 15000.0\n")
+                filehandler_cfg.write("brightness = 94\n")
+                filehandler_cfg.write("image_prefix = %s\n" % row[0])
+                filehandler_cfg.write("\n")
+                
+filehandler_cfg.close()
 
-#import csv
-#print "STAR_STATUS = {"
-#with open('mapping.csv', 'rb') as csvfile:
-#    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-#    for row in spamreader:
-#        if row[1] == "name" or row[1].__contains__('m') or row[1] == "" or row[2] == "NA"  or row[2] == "GND":
-#            pass
-#        else:
-#            print '"%s":["%s","%s"],' % (row[1],row[2],"OFF")
-#print "}"
-#
+filehandler_dic.write("STAR_STATUS = {")
+lines = ""
+with open('mapping.csv', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        if row[1] == "name" or row[1].__contains__('m') or row[1] == "" or row[2] == "NA"  or row[2] == "GND":
+            pass
+        else:
+            lines += '"%s":["%s","%s"],\n' % (row[1],row[2],"OFF")
+lines += "}\n"
+lines = lines.replace(",\n}","}")
+filehandler_dic.write(lines)
+filehandler_dic.close()
+
+os.rename(filename_cfg,"../BeagleDarc/BeagleDarc/%s"%filename_cfg)
+os.rename(filename_dic,"../BBBServer/%s"%filename_dic)
