38bee84684be935af417ba90cf56179a3d3973ac
Norman Saez <nfsaez@uc.cl>
2014-03-10 22:22:23 -0300

    fixing controller

--- BeagleDarc/BeagleDarc/Controller.py
+++ BeagleDarc/BeagleDarc/Controller.py
@@ -25,7 +25,8 @@ class Controller:
         orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID)
         
         # Obtain a reference to the root naming context
-        obj         = orb.resolve_initial_references("NameService")
+#        obj         = orb.resolve_initial_references("NameService")
+        obj         = orb.string_to_object("corbaloc:iiop:10.42.0.97/NameService")
         rootContext = obj._narrow(CosNaming.NamingContext)
         
         if rootContext is None:
@@ -132,5 +133,7 @@ if __name__ == '__main__':
     c = Controller()
     c.star_on(1)
     c.star_off(1)
-    print c.layer_move('ground_layer')
-    print c.layer_move_skip_sensor('ground_layer')
+    #print c.layer_move('ground_layer')
+    #print c.layer_move_skip_sensor('ground_layer')
+    import darc
+    c = darc.Control('ShackHartmann')
